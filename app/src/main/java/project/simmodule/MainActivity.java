20bcc509fe2375be3ea73989f44d10aa10b51c68
GARUDA <lanka.naveen.emn@gmail.com>
2017-04-30 21:55:59 -0700

    Initial commit

---
+++ app/src/main/java/project/simmodule/MainActivity.java
@@ -0,0 +1,20 @@
+package project.simmodule;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MainActivity extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context c, Intent i)
+		{
+		
+			Intent a=new Intent(c, Sendmsg.class);
+			a.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			c.startActivity(a);			
+		}
+	}
+	
+
+
+
---
+++ app/src/main/java/project/simmodule/Sendmsg.java
@@ -0,0 +1,45 @@
+package project.simmodule;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Sendmsg extends Activity {
+
+	SharedPreferences sp=null;
+	SharedPreferences.Editor edit;
+	TelephonyManager telemananger=null;
+	TelephonyManager telemanager2=null;
+	String anothervar=null;
+	String var=null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		telemananger = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+		 anothervar = telemananger.getSimSerialNumber();
+		 
+		sp=getSharedPreferences("share1",Activity.MODE_APPEND);
+		edit=sp.edit();
+		
+		var=sp.getString("sim_no",null);
+		if(var==null)
+		{
+		
+		sp.edit().putString("sim_no",anothervar).commit();
+		var=sp.getString("sim_no",null);
+		TextView tv=(TextView)findViewById(R.id.textView2);
+		tv.setText(String.valueOf(anothervar));
+		}
+		else
+		Toast.makeText(this, "hi..", Toast.LENGTH_LONG).show();
+		TextView tv=(TextView)findViewById(R.id.textView4);
+		tv.setText(String.valueOf("this is the value: "+var));
+		
+		}	
+		
+	}
